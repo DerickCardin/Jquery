e92e9331a7599838b331809a743bc097f466a9f4
Derick Cardin <derickc50@gmail.com>
2018-08-18 18:36:15 +0000

    Lesson 4-6

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    alert("jquery loaded");
-});
+//Lesson 5 Code
+//$("h3").css({border: "3px solid blue"});
+//$(".wrapper").css({border: "3px solid red"});
+//$("#clients").css({border: "3px solid yellow"});
+
+//Lesson 6
+//$("header nav li:first").css({border: "2px solid red"});
+//$("header nav li:last").css({border: "2px solid red"});
+
+//$("#contact ul:first").css({border: "2px solid red"});
+//$("#contact ul:last").css({border: "2px solid red"});
+
+//$("header nav li:even").css({border: "2px solid blue"});
+//$("header nav li:odd").css({border: "2px solid blue"});
+
+//$("section:not('#contact')").css({border: "2px solid green"});
+
+//$("#solcial-nav li:lt(3)").css({border: "2px solid blue"});
+//$("#solcial-nav li:gt(2)").css({border: "2px solid blue"});
+
+//$("div[class]").css({border: "2 px solid pink"});
+
+//$("img[alt=quote]").css({border: "2px solid purple"});
+
