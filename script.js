c4478175d8c053d92f81925773364c755669bf3b
Derick Cardin <derickc50@gmail.com>
2018-08-18 20:34:24 +0000

    Lesson19-21

--- script.js
+++ script.js
@@ -184,6 +184,87 @@
 
 
 
+//Lesson 19
+//$(document).ready(function(){
+
+//function complete(){
+//   alert("Animation complete");
+//}
+ 
+// $("section > h2").on("click", function(){
+  
+//  $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete);
+   
+//  });
+// });
+ 
+
+//Lesson 20
+
+//$(document).ready(function(){
+ 
+// $("section > h2").on("click", function(){
+  
+//  $(this).fadeOut(2000).fadeIn(500);
+// $(this).fadeTo(200, 0.2)
+//        .fadeTo(200, 0.8)
+//        .fadeTo(200, 0.2)
+  //      .fadeTo(200, 0.8)
+    //    .fadeTo(200, 0.2)
+      //  .fadeTo(200, 0.8);
+  
+  
+// });
+ 
+//});
+
+//Lesson 21
+
+//$(document).ready(function(){
+ //$("img[alt=map]").on("click", function(){
+  
+  //$(this).hide(1000).show(1000);
+ // $("section > h2").toggle(1000);
+// });
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
