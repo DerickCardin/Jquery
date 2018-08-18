b6d64ff811847ffe6ad0a11587db8049253d5082
Derick Cardin <derickc50@gmail.com>
2018-08-18 19:31:14 +0000

    Lesson10-12

--- script.js
+++ script.js
@@ -61,3 +61,66 @@
 //.after()
 //.html()
 //.text()
+
+
+
+//Lesson 10
+
+//$("section").wrap("div");
+//$("section").unwrap();
+//$("section").wrapAll("div");
+
+//var wrapper = "<div class='wrapper'>";
+//var button = $(".button");
+//var wrapped = true;
+
+//button[0].onclick = function(){
+ // if (wrapped){
+ //     $("section").unwrap();
+ //     wrapped = false;
+ //     button.text("Wrap");
+ // }  else{
+  //    $("section").wrapAll(wrapper);
+//      wrapped = true;
+ //   button.text("Wrapped");  
+ //     
+ // }
+    
+//};
+
+
+
+
+
+//wrap()
+//unwrap()
+//wrapAll()
+
+
+
+
+//Lesson 11
+//.empty()
+//.remove()
+
+//$(".button").empty();
+//$("points-of-sale").empty();
+//$(".button").remove();
+//$("#contact img").remove();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
