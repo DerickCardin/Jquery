147ee9c4ef8c24a204ca1c9894104048a4fdd64c
Derick Cardin <derickc50@gmail.com>
2018-08-18 21:04:27 +0000

    Lesson 22-24

--- script.js
+++ script.js
@@ -229,8 +229,73 @@
 //});
 
 
+//Lesson 22
 
+//$(document).ready(function(){
+// $(".slide-button-up").on("click", function(){
+//  $("#lead-banner").slideUp(100);
+ 
+  
+// });
+//  $(".slide-button-up").on("click", function(){
+//  $("#lead-banner").slideToggle(1000, function(){
+//   alert("animation complete");
+//  });
+ 
+  
+// });
+ 
+// $(".slide-button-down").on("click", function(){
+//  $("#lead-banner").slideDown(4000, function(){
+//   alert("animation complete");
+//  });
+ 
+  
+// });
+ 
+//});
 
+//Lesson 23
+
+//$(document).ready(function(){
+ 
+// var allQuotes = $("blockquote");
+// var currentQuote = 0;
+ 
+// function changeQuote(){
+  
+ // $(allQuotes[currentQuote]).fadeOut(200, funtion(){
+   
+  
+ // if(currentQuote == allQuotes.length - 1){
+ //  urrentQuote = 0;
+   
+ // } else {
+ //  currentQuote++;
+ // }
+  
+ // $(allQuotes[currentQuote]).fadeIn(200);
+ //} 
+   
+ // });
+  
+ 
+ //var quoteTimer = setInterval(changeQuote, 3000);
+ 
+//});
+
+//Lesson 24
+
+//$(document).ready(function(){
+ 
+ //var items = $("#points-of-sale li");
+ //items.on("click", function(){
+  
+ // $(this).find("p").slideToggle(500);
+  
+// });
+ 
+//});
 
 
 
