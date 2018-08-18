6581fe9f4d5b6f405cf630900a69e858d6a37f02
Derick Cardin <derickc50@gmail.com>
2018-08-18 20:10:36 +0000

    Lesson 16-18

--- script.js
+++ script.js
@@ -156,4 +156,46 @@
 
 
 
+//Lesson 16
+//$("#lead-banner").dlbclick(function(){
+// alert("you clicked me");
+// $("#lead-banner").off("dblclick");
+//});
+
+//Lesson 17
+//$(window).load(function(){
+// 
+// });
+
+//Lesson 18
+//$(document).ready(function(){
+ 
+// $("*").on("click", function(e){
+  
+//  console.log(e.target);
+//  console.log("The event type is: " + e.type);
+//  console.log("x co-ordinate of the event is:" + e.pageX);
+//  console.log("y co-ordinate of the event is:" + e.pageY);
+//  e.stopPropagation();
+  
+// });
+ 
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
